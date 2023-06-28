f73c481822250ffb5bd139acbaf5bdb398bffecd
rurigrass <ruripg@gmail.com>
2023-06-28 23:04:29 +0200

    votes route handle add cache

--- src/app/api/company/spill/vote/route.ts
+++ src/app/api/company/spill/vote/route.ts
@@ -2,6 +2,8 @@ import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { SpillVoteValidator } from "@/lib/validators/vote";
 
+const CACHE_AFTER_UPVOTES = 1
+
 export async function PATCH(req: Request) {
   try {
     //check user is logged in or give an error
@@ -13,16 +15,8 @@ export async function PATCH(req: Request) {
     //gets the body from req
     const body = req.json();
 
-    //Type Safe Validation
+    //Type Safe Validation - the spillID and whether its up or down
     const { spillId, voteType } = SpillVoteValidator.parse(body);
-
-    //check if there is a vote there already / find existing vote
-    const existingVote = await db.vote.findFirst({
-      where: {
-        userId: session.user.id,
-        spillId,
-      },
-    });
 
     //get the spill + spill's author and votes
     const spill = await db.spill.findUnique({
@@ -40,7 +34,16 @@ export async function PATCH(req: Request) {
       return new Response("Spill not found", { status: 404 });
     }
 
+    //check if there is a vote there already / find existing vote
+    const existingVote = await db.vote.findFirst({
+      where: {
+        userId: session.user.id,
+        spillId,
+      },
+    });
+
     if (existingVote) {
+      //if clicked, unclick it (delete)
       if (existingVote.type === voteType) {
         await db.vote.delete({
           where: {
@@ -50,8 +53,30 @@ export async function PATCH(req: Request) {
             },
           },
         });
+        return new Response("OK");
       }
-      return new Response("OK");
+      // if clicked is different from what has already been clicked
+      await db.vote.update({
+        where: {
+          userId_spillId: {
+            spillId,
+            userId: session.user.id,
+          },
+        },
+        data: {
+          type: voteType,
+        },
+      });
+
+      //if interactivity is too high we should cache it / recount the votes
+      const votesAmount = spill.votes.reduce((acc, vote) => {
+        if (vote.type === "UP") return acc + 1;
+        if (vote.type === "DOWN") return acc - 1;
+        return acc;
+      }, 0);
+      if(votesAmount>=CACHE_AFTER_UPVOTES) {
+        
+      }
     }
   } catch (error) {}
 }
