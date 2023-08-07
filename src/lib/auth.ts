076a958cc7e30fc5bfdae95cfd807028da7af937
rurigrass <ruripg@gmail.com>
2023-08-07 16:31:09 +0100

    default usegoogle image in schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -26,8 +26,6 @@ export const authOptions: NextAuthOptions = {
         session.user.email = token.email;
         session.user.image = token.picture;
         session.user.username = token.username;
-        // session.user.use
-        // if (token.picture)
       }
       return session;
     },
